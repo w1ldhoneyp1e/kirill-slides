0d6d5b523e2757f6be7e25ff821ab5dd1cb2d6d7
w1ldhoneyp1e <k_yashmetov@mail.ry>
2024-11-01 13:01:01 +0300

    Реализовал drag and drop в списке слайдов, только проблема с размером слайда - высота при перетаскивании сводится к размеру контента

--- src/view/Collection/Shell/Shell.tsx
+++ src/view/Collection/Shell/Shell.tsx
@@ -1,9 +1,9 @@
 import { useState, useEffect, useRef, RefObject } from 'react'
 import styles from './Shell.module.css'
 import { Slide } from '../../../components/Slide/Slide'
+import { EditorType, PositionType, SlideType } from '../../../store/types'
 import { dispatch } from '../../../store/editor'
 import { changeSlidePosition } from '../../../store/methods'
-import { EditorType, PositionType, SlideType } from '../../../store/types'
 
 type ShellProps = {
 	editor: EditorType
@@ -14,93 +14,82 @@ type ShellProps = {
 function Shell({ editor, onClick, slide }: ShellProps) {
 	const slideRef = useRef<HTMLDivElement>(null)
 	const [position, setPosition] = useState<PositionType | null>({ x: 0, y: 0 })
-	const [initialPosition, setInitialPosition] = useState<PositionType | null>(
-		null,
-	)
 	const [isDragging, setDragging] = useState<boolean>(false)
-	const [dragId, setDragId] = useState<string | null>(null)
 	const [startPos, setStartPos] = useState<PositionType | null>(null)
+	const [modelStartPos, setModelStartPos] = useState<PositionType | null>(null)
 	const [dragIndex, setDragIndex] = useState<number | null>(null)
 	const SLIDE_HEIGHT = (300 * 9) / 16
 
 	const collection = [...editor.presentation.slides]
 
 	function useDragAndDrop(ref: RefObject<HTMLDivElement>) {
-		const handleMouseDown = (
-			e: MouseEvent,
-			slideId: string,
-			index: number,
-		): void => {
-			setStartPos({ x: e.clientX, y: e.clientY })
-			setDragId(slideId)
+		const handleMouseDown = (e: MouseEvent): void => {
+			const index = collection.findIndex((s) => s.id === slide.id)
 			setDragIndex(index)
-			if (position) {
-				setInitialPosition({ x: position.x, y: position.y }) // Сохраняем текущее положение
-			}
+			setStartPos({ x: e.pageX, y: e.pageY })
+			ref.current
+				? setModelStartPos({
+						x: ref.current.getBoundingClientRect().left,
+						y: ref.current.getBoundingClientRect().top,
+				  })
+				: setModelStartPos({ x: 0, y: 0 })
 			setDragging(true)
 			e.preventDefault() // предотвращает выделение текста
 		}
 
 		const handleMouseMove = (e: MouseEvent) => {
-			if (isDragging && initialPosition && startPos) {
-				const deltaX = e.clientX - startPos.x
-				const deltaY = e.clientY - startPos.y
-
-				// Обновляем позицию слайда
-				setPosition({
-					x: initialPosition.x + deltaX,
-					y: initialPosition.y + deltaY,
-				})
-
-				// Проверка на перемещение слайда
-				const deltaYAbs = Math.abs(deltaY)
-				if (deltaYAbs > SLIDE_HEIGHT && dragIndex) {
-					const targetIndex = deltaY > 0 ? dragIndex + 1 : dragIndex - 1
-					if (targetIndex >= 0 && targetIndex < collection.length) {
-						dispatch(changeSlidePosition, {
-							slideId: dragId,
-							positionToMove: targetIndex,
-						})
-						setDragIndex(targetIndex)
-						setStartPos({ x: e.clientX, y: e.clientY }) // обновляем начальную позицию
-						if (position) {
-							setInitialPosition({
-								x: position.x + deltaX,
-								y: position.y + deltaY,
-							}) // обновляем начальное положение
-						}
-					}
+			const delta = startPos
+				? { x: e.pageX - startPos.x, y: e.pageY - startPos.y }
+				: { x: 0, y: 0 }
+			const newPos = modelStartPos
+				? {
+						x: modelStartPos.x + delta.x,
+						y: modelStartPos.y + delta.y,
+				  }
+				: { x: 0, y: 0 }
+			setPosition(newPos)
+			const deltaYAbs = Math.abs(delta.y)
+			if (deltaYAbs > SLIDE_HEIGHT && dragIndex !== null) {
+				const targetIndex = delta.y > 0 ? dragIndex + 1 : dragIndex - 1
+				if (targetIndex >= 0 && targetIndex < collection.length) {
+					dispatch(changeSlidePosition, {
+						slideId: slide.id,
+						positionToMove: targetIndex,
+					})
+					setDragIndex(targetIndex)
+					// Сбрасываем стартовую позицию и начальное положение модели
+					setStartPos({ x: e.pageX, y: e.pageY })
+					setModelStartPos(newPos)
 				}
 			}
+			e.preventDefault() // предотвращает выделение текста
 		}
 
 		const handleMouseUp = () => {
+			if (ref.current) {
+				ref.current.removeEventListener('mousedown', handleMouseDown)
+			}
+			document.removeEventListener('mousemove', handleMouseMove)
+			document.removeEventListener('mouseup', handleMouseUp)
 			setDragIndex(null)
-			setDragId(null)
 			setStartPos(null)
-			setInitialPosition(null)
 			setDragging(false)
 		}
 
 		useEffect(() => {
-			const index = collection.findIndex((s) => s.id === slide.id)
 			if (ref.current) {
-				ref.current.addEventListener('mousedown', (e) =>
-					handleMouseDown(e, slide.id, index),
-				)
+				ref.current.addEventListener('mousedown', handleMouseDown)
 			}
 
-			window.addEventListener('mousemove', handleMouseMove)
-			window.addEventListener('mouseup', handleMouseUp)
+			document.addEventListener('mousemove', handleMouseMove)
+			document.addEventListener('mouseup', handleMouseUp)
 
 			return () => {
 				if (ref.current) {
-					ref.current.removeEventListener('mousedown', (e) =>
-						handleMouseDown(e, slide.id, index),
-					)
+					ref.current.removeEventListener('mousedown', handleMouseDown)
 				}
-				window.removeEventListener('mousemove', handleMouseMove)
-				window.removeEventListener('mouseup', handleMouseUp)
+				document.removeEventListener('mousemove', handleMouseMove)
+				document.removeEventListener('mouseup', handleMouseUp)
 			}
 		}, [slide.id, collection])
 	}
@@ -117,7 +106,7 @@ function Shell({ editor, onClick, slide }: ShellProps) {
 				position: isDragging ? 'absolute' : 'static',
 				top: isDragging && position ? `${position.y}px` : 'auto',
 				left: isDragging && position ? `${position.x}px` : 'auto',
-				transition: isDragging ? 'none' : 'top 0.2s, left 0.2s', // Плавный переход при завершении перетаскивания
+				// transition: isDragging ? 'none' : 'top 0.2s, left 0.2s', // Плавный переход при завершении перетаскивания
 			}}
 		>
 			<Slide
