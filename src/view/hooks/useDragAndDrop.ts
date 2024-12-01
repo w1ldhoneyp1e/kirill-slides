4afb09700e04954a576efc9ea504a4d368c1ca96
w1ldhoneyp1e <k_yashmetov@mail.ry>
2024-12-01 10:12:41 +0300

    Поправил dragAndDrop по совету линтера

--- src/view/hooks/useDragAndDrop.ts
+++ src/view/hooks/useDragAndDrop.ts
@@ -21,57 +21,56 @@ function useDragAndDrop({
     const startPos = useRef<PositionType | null>(null)
     const modelStartPos = useRef<PositionType | null>(null)
 
-    const handleMouseDown = (e: MouseEvent): void => {
-        onMouseDown()
-        startPos.current = {
-            x: e.pageX,
-            y: e.pageY,
-        }
-        modelStartPos.current = ref.current
-            ? {
-                x: ref.current.getBoundingClientRect().left,
-                y: ref.current.getBoundingClientRect().top,
-            }
-            : {
-                x: 0,
-                y: 0,
-            }
-
-        document.addEventListener('mousemove', handleMouseMove)
-        document.addEventListener('mouseup', handleMouseUp)
-        e.preventDefault()
-    }
-
-    const handleMouseMove = (e: MouseEvent) => {
-        if (!startPos.current || !modelStartPos.current || !parentRef.current) return
-
-        const parentRect = parentRef.current.getBoundingClientRect()
-
-        const delta = {
-            x: e.pageX - startPos.current.x,
-            y: e.pageY - startPos.current.y,
-        }
-
-        const newDelta = {
-            x: modelStartPos.current.x + delta.x - parentRect.left,
-            y: modelStartPos.current.y + delta.y - parentRect.top,
-        }
-
-        deltaRef.current = newDelta
-        setDelta(newDelta)
-        e.preventDefault()
-    }
-
-    const handleMouseUp = () => {
-        document.removeEventListener('mousemove', handleMouseMove)
-        document.removeEventListener('mouseup', handleMouseUp)
-
-        if (deltaRef.current !== null) {
-            onMouseUp(deltaRef.current)
-        }
-    }
-
     useEffect(() => {
+        const handleMouseMove = (e: MouseEvent) => {
+            if (!startPos.current || !modelStartPos.current || !parentRef.current) return
+
+            const parentRect = parentRef.current.getBoundingClientRect()
+
+            const delta = {
+                x: e.pageX - startPos.current.x,
+                y: e.pageY - startPos.current.y,
+            }
+
+            const newDelta = {
+                x: modelStartPos.current.x + delta.x - parentRect.left,
+                y: modelStartPos.current.y + delta.y - parentRect.top,
+            }
+
+            deltaRef.current = newDelta
+            setDelta(newDelta)
+            e.preventDefault()
+        }
+
+        const handleMouseUp = () => {
+            document.removeEventListener('mousemove', handleMouseMove)
+            document.removeEventListener('mouseup', handleMouseUp)
+
+            if (deltaRef.current !== null) {
+                onMouseUp(deltaRef.current)
+            }
+        }
+        const handleMouseDown = (e: MouseEvent): void => {
+            onMouseDown()
+            startPos.current = {
+                x: e.pageX,
+                y: e.pageY,
+            }
+            modelStartPos.current = ref.current
+                ? {
+                    x: ref.current.getBoundingClientRect().left,
+                    y: ref.current.getBoundingClientRect().top,
+                }
+                : {
+                    x: 0,
+                    y: 0,
+                }
+
+            document.addEventListener('mousemove', handleMouseMove)
+            document.addEventListener('mouseup', handleMouseUp)
+            e.preventDefault()
+        }
+
         const element = ref.current
         if (element) {
             element.addEventListener('mousedown', handleMouseDown)
@@ -81,7 +80,7 @@ function useDragAndDrop({
                 element.removeEventListener('mousedown', handleMouseDown)
             }
         }
-    }, [ref])
+    }, [onMouseDown, onMouseUp, parentRef, ref])
 
     return delta
 }
