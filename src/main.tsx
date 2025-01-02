96460233ab1925bae87e681073bf9367fd0ee569
w1ldhoneyp1e <k_yashmetov@mail.ry>
2025-01-02 18:36:54 +0300

    Вернул стиль из корня

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {Provider} from 'react-redux'
 import App from './App'
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
+import './index.css'
 import {loadFromLocalStorage} from './store/localStorage/loadFromLocalStorage'
 import {setEditor} from './store/redux/editorActionCreators.ts'
 import {store} from './store/redux/store.ts'
